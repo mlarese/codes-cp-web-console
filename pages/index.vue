0b28635e6bfc9e9eb3175df1d4d7fad71c3526e6
mlarese <mauro.larese@gmail.com>
2019-02-22 17:20:01 +0000

    store updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,18 @@
 <template>
   <layout>
     <div><list-code-cp/></div>
-    <div class="my-5"><ViewCodeCp/></div>
   </layout>
 </template>
 
 <script>
 import ListCodeCp from '../components/codecp/ListCodeCp'
-import ViewCodeCp from '../components/codecp/ViewCodeCp'
 const root = {root: true}
 export default {
   components: {
-        ListCodeCp,
-        ViewCodeCp
+        ListCodeCp
   },
-    fetch ({store}) {
-        store.dispatch('code/load', {}, root)
-    }
+    // fetch ({store}) {
+    //    store.dispatch('code/load', {}, root)
+   // }
 }
 </script>
